0398c2439fd01874782cb3cbbec71cb362e853f6
PhuongNM4 <live_red_river@yahoo.com>
2014-04-27 17:13:39 +0700

    1

---
+++ javascripts/ChatApp.js
@@ -0,0 +1,45 @@
+﻿
+angular.element(document).ready(function () {
+    $('.messageText').emotions();
+});
+
+var app = angular.module('myApp', ['firebase']);
+
+function MyController($scope, $firebase) {
+
+    var proxy = new Firebase("https://fdn-phuongnm4.firebaseio.com");
+
+    $scope.disableName = false;
+    $scope.messages = $firebase(proxy);
+
+    $scope.addMessage = function (e) {
+        if (e.keyCode != 13) return;
+
+        if (!$scope.disableName && $scope.name != "") {
+            $scope.disableName = true;
+        }
+        $scope.timestamp = GetDateTimeNow();
+        var newId = $('.messageText').length + 1;
+
+        $scope.messages.$add({
+            id: newId,
+            from: $scope.name,
+            body: $scope.msg,
+            timestamp: $scope.timestamp
+        });
+
+        $scope.msg = "";
+    };
+}
+
+function GetDateTimeNow() {
+    var date = new Date();
+    return AppendZero(date.getHours()) + ":" + AppendZero(date.getMinutes()) + "...." + AppendZero(date.getDate()) + "-" + AppendZero((date.getMonth() + 1)) + "-" + (date.getYear() + 1900);
+}
+
+function AppendZero(input) {
+    if (input.toString().length < 2) {
+        return "0".concat(input);
+    }
+    return input;
+}
