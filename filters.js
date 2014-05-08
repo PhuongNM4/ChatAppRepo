e24e5f3e1c52ea4084b2f2aa0399adaa12c0563f
PhuongNM4 <live_red_river@yahoo.com>
2014-05-08 18:34:23 +0700

    Create filters.js

---
+++ filters.js
@@ -0,0 +1,18 @@
+
+angular.module('codeblockFilters', []).filter('splitTags', function () {
+
+    return function (input) {
+
+        debugger
+
+        var arrStr = input.split(',');
+        var returnStr = "";
+        for (var i = 0; i < arrStr.length; i++) {
+            returnStr = returnStr + '<code>' + arrStr[i] + '</code>';
+        }
+        return returnStr;
+    };
+})
+
+
+
